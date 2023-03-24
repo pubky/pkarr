3336e863f94dcd7c4fe7787c15a5bdc7cebbe2bf
nazeh <ar.nazeh@gmail.com>
2023-03-24 12:29:26 +0300

    feat(server): add GET /pkarr/:hash

--- js/server/index.js
+++ js/server/index.js
@@ -1,33 +1,88 @@
-import DHT from 'bittorrent-dht'
-import sodium from 'sodium-native'
+import http from 'http';
+import url from 'url';
+import DHT from 'bittorrent-dht';
+import ed from 'bittorrent-dht-sodium'
 
-const dht = new DHT({
-  verify: sodium.crypto_sign_verify_detached
-})
+const dht = new DHT({ verify: ed.verify  });
 
-    console.log(dht.table)
-dht.get(
-  Buffer.from('3742a9a6bed50ea6899269623af5b5431d22f905', 'hex'), 
-  function (err, hash) {
-    console.log(err, hash)
+const server = http.createServer(async (req, res) => {
+  boilerplate(req, res);
+
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname.split('/');
+  const hash = path[2];
+
+  if (!path[1] === 'pkarr' || !hash) {
+    res.statusCode = 404;
+    res.end('Not found');
+    return
   }
-)
 
-// Optionally pass an exisiting secret key and rederive the public key
-function generateKeyPair (secretKey) {
-  const publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
-  if (secretKey == null) {
-    secretKey = sodium.sodium_malloc(sodium.crypto_sign_SECRETKEYBYTES)
-    sodium.crypto_sign_keypair(publicKey, secretKey)
+  // Handle GET /pkarr/:hash
+  if (req.method === 'GET') {
+    dht.get(hash, (err, response) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Failed to fetch value from DHT');
+        return;
+      }
+
+      if (!response) {
+        res.statusCode = 404;
+        res.end('Value not found');
+        return;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({
+		    k: response.k.toString('hex'),
+		    s: response.seq || 0,
+		    v: response.v.toString('hex'),
+	    }));
+    });
+  // Handle PUT /pkarr/:hash
+  } else if (req.method === 'PUT') {
+    // let body = '';
+    //
+    // req.on('data', chunk => {
+    //   body += chunk;
+    // });
+    //
+    // req.on('end', async () => {
+    //   const payload = JSON.parse(body);
+    //
+    //   dht.put({ v: payload.value }, (err, hash) => {
+    //     if (err) {
+    //       res.statusCode = 500;
+    //       res.end('Failed to put value into DHT');
+    //       return;
+    //     }
+    //
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.end(JSON.stringify({ hash: hash.toString('hex') }));
+    //   });
+    // });
   } else {
-    sodium.crypto_sign_ed25519_sk_to_pk(publicKey, secretKey)
+    res.statusCode = 405;
+    res.end('Method not allowed');
   }
+});
 
-  return { publicKey, secretKey }
-}
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
-function sign (msg) {
-  const sig = Buffer.alloc(sodium.crypto_sign_BYTES)
-  sodium.crypto_sign_detached(sig, msg, keyPair.secretKey)
-  return sig
+function boilerplate (req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Preflight request. Reply successfully:
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
 }
