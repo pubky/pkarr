303886e5be5591a48fa33ebf4200d375da5bae5e
nazeh <ar.nazeh@gmail.com>
2023-09-02 21:05:37 +0300

    update benchmarks to be more representative of typical usecase

--- js/benchmarks/README.md
+++ js/benchmarks/README.md
@@ -16,10 +16,10 @@ Results:
  |--------------------|------------|----------|------------|
  | json + brotli      | 0.48       | 1 ms     | 1 ms       |
  | bencode + lz4      | 0.52       | 0 ms     | 1 ms       |
- | bencode + brotli   | 0.34       | 25 ms    | 1 ms       |
- | csv + brotli       | 0.33       | 13 ms    | 1 ms       |
- | csv + lz4          | 0.49       | 1 ms     | 1 ms       |
- | custom + brotli    | 0.31       | 13 ms    | 3 ms       |
+ | csv + lz4          | 0.49       | 0 ms     | 1 ms       |
+ | bencode + brotli   | 0.34       | 26 ms    | 0 ms       |
+ | csv + brotli       | 0.33       | 20 ms    | 1 ms       |
+ | custom + brotli    | 0.31       | 14 ms    | 4 ms       |
  └--------------------┴------------┴----------┴------------┘
 ```
 
@@ -29,4 +29,19 @@ Conclusion:
 
 `csv + brotli` seems to offer the best compression, fast decoding, and acceptable encoding time.
 
-`custom + brotli` Where we encode dns type by its code dosen't offer much benifit, it needs two bytes, which only saves 1 byte for `TXT` and 2 for `AAAA`, while complecating decoding. 
+`custom + brotli` Where we encode dns type by its code ~dosen't offer much benifit, it needs two bytes, which only saves 1 byte for `TXT` and 2 for `AAAA`, while complecating decoding.~ 
+
+Actually encoding ipv4 and ipv6 for what is mostly `A`, `AAAA`, and `TXT` records, it starts to pay off.
+
+```
+ ┌--------------------┬------------┬----------┬------------┐
+ | TYPE               | SIZE RATIO | COMPRESS | DECOMPRESS |
+ |--------------------|------------|----------|------------|
+ | json + brotli      | 0.97       | 1 ms     | 1 ms       |
+ | bencode + lz4      | 1.01       | 0 ms     | 1 ms       |
+ | csv + lz4          | 0.96       | 0 ms     | 1 ms       |
+ | bencode + brotli   | 0.78       | 21 ms    | 0 ms       |
+ | csv + brotli       | 0.77       | 10 ms    | 0 ms       |
+ | custom + brotli    | 0.60       | 13 ms    | 5 ms       |
+ └--------------------┴------------┴----------┴------------┘
+ ```
--- js/benchmarks/compression-vs-encoding.js
+++ js/benchmarks/compression-vs-encoding.js
@@ -2,50 +2,21 @@ import lz4 from 'lz4'
 import brotli from 'brotli-compress'
 import assert from 'assert'
 import bencode from 'bencode'
+import * as ipCodec from '@leichtgewicht/ip-codec'
 
 // https://www.lucidchart.com/techblog/2019/12/06/json-compression-alternative-binary-formats-and-compression-methods/
 
-const csv = `
-cloudflare.com.,300,IN,CAA,0 issuewild "digicert.com; cansignhttpexchanges=yes"
-cloudflare.com.,146,IN,A,104.16.132.229
-cloudflare.com.,146,IN,A,104.16.133.229
-cloudflare.com.,97,IN,AAAA,2606:4700::6810:85e5
-cloudflare.com.,97,IN,AAAA,2606:4700::6810:84e5
-cloudflare.com.,44965,IN,NS,ns5.cloudflare.com.
-cloudflare.com.,44965,IN,NS,ns7.cloudflare.com.
-cloudflare.com.,44965,IN,NS,ns6.cloudflare.com.
-cloudflare.com.,44965,IN,NS,ns4.cloudflare.com.
-cloudflare.com.,44965,IN,NS,ns3.cloudflare.com.
-cloudflare.com.,1800,IN,MX,5 mailstream-canary.mxrecord.io.
-cloudflare.com.,1800,IN,MX,20 mailstream-central.mxrecord.mx.
-cloudflare.com.,1800,IN,MX,10 mailstream-east.mxrecord.io.
-cloudflare.com.,1800,IN,MX,10 mailstream-west.mxrecord.io.
-cloudflare.com.,300,IN,TXT,"google-site-verification=ZdlQZLBBAPkxeFTCM1rpiB_ibtGff_JF5KllNKwDR9I"
-cloudflare.com.,300,IN,TXT,"ZOOM_verify_7LFBvOO9SIigypFG2xRlMA"
-cloudflare.com.,300,IN,TXT,"docker-verification=c578e21c-34fb-4474-9b90-d55ee4cba10c"
-cloudflare.com.,300,IN,TXT,"atlassian-domain-verification=WxxKyN9aLnjEsoOjUYI6T0bb5vcqmKzaIkC9Rx2QkNb751G3LL/cus8/ZDOgh8xB"
-cloudflare.com.,300,IN,TXT,"miro-verification=bdd7dfa0a49adfb43ad6ddfaf797633246c07356"
-cloudflare.com.,300,IN,TXT,"google-site-verification=C7thfNeXVahkVhniiqTI1iSVnElKR_kBBtnEHkeGDlo"
-cloudflare.com.,300,IN,TXT,"status-page-domain-verification=r14frwljwbxs"
-cloudflare.com.,300,IN,TXT,"drift-domain-verification=f037808a26ae8b25bc13b1f1f2b4c3e0f78c03e67f24cefdd4ec520efa8e719f"
-cloudflare.com.,300,IN,TXT,"apple-domain-verification=DNnWJoArJobFJKhJ"
-cloudflare.com.,300,IN,TXT,"logmein-verification-code=b3433c86-3823-4808-8a7e-58042469f654"
-cloudflare.com.,300,IN,TXT,"stripe-verification=5096d01ff2cf194285dd51cae18f24fa9c26dc928cebac3636d462b4c6925623"
-cloudflare.com.,300,IN,TXT,"MS=ms70274184"
-cloudflare.com.,300,IN,TXT,"facebook-domain-verification=h9mm6zopj6p2po54woa16m5bskm6oo"
-cloudflare.com.,300,IN,TXT,"v=spf1 ip4:199.15.212.0/22 ip4:173.245.48.0/20 include:_spf.google.com include:spf1.mcsv.net include:spf.mandrillapp.com include:mail.zendesk.com include:stspg-customer.com include:_spf.salesforce.com -all"
-cloudflare.com.,300,IN,TXT,"cisco-ci-domain-verification=27e926884619804ef987ae4aa1c4168f6b152ada84f4c8bfc74eb2bd2912ad72"
-cloudflare.com.,300,IN,TXT,"stripe-verification=bf1a94e6b16ace2502a4a7fff574a25c8a45291054960c883c59be39d1788db9"
-cloudflare.com.,300,IN,CAA,0 issue "digicert.com; cansignhttpexchanges=yes"
-cloudflare.com.,300,IN,CAA,0 issuewild "digicert.com; cansignhttpexchanges=yes"
-cloudflare.com.,300,IN,CAA,0 issue "letsencrypt.org"
-cloudflare.com.,300,IN,CAA,0 issuewild "comodoca.com"
-cloudflare.com.,300,IN,CAA,0 issue "comodoca.com"
-cloudflare.com.,300,IN,CAA,0 iodef "mailto:tls-abuse@cloudflare.com"
-cloudflare.com.,300,IN,CAA,0 issuewild "letsencrypt.org"
-cloudflare.com.,300,IN,CAA,0 issuewild "pki.goog; cansignhttpexchanges=yes"
-cloudflare.com.,300,IN,CAA,0 issue "pki.goog; cansignhttpexchanges=yes"
+const TYPICAL = `
+@,100,IN,A,104.16.132.229
+fedimint.@,97,IN,AAAA,2606::4700::6810::84e5
+router.@,97,IN,AAAA,2606:2800:220:1:248:1893:25c8:19
+@,97,IN,CNAME,nuh.dev
+_atproto.@,300,IN,TXT,"did=did:plc:zki2d3v7nckil3azsodv72sy"
+_matrix.@,1080,IN,TXT,"handle=@nuhvi:matrix.org"
 `
+
+const csv = TYPICAL
+
 const records = csvToRecords(csv)
 
 const BASE = csv.length
@@ -100,8 +71,27 @@ const bencoded = bencode.encode(records)
   }
 }
 
+// LZ4 CSV
 {
-  // Brotli bencode
+  const compressStart = Date.now()
+  const compressed = lz4.encode(csv)
+  const compressEnd = Date.now()
+
+  const decompressStart = Date.now()
+  const decompressed = csvToRecords(lz4.decode(compressed).toString())
+  const decompressEnd = Date.now()
+
+  assert(decompressed.length === records.length)
+
+  results['csv + lz4'] = {
+    sizeRatio: sizeRatio(compressed),
+    compressTime: compressEnd - compressStart,
+    decompressTime: decompressEnd - decompressStart
+  }
+}
+
+// Brotli bencode
+{
   const compressStart = Date.now()
   const compressed = await brotli.compress(bencoded)
   const compressEnd = Date.now()
@@ -144,25 +134,6 @@ const bencoded = bencode.encode(records)
   }
 }
 
-// LZ4 CSV
-{
-  const compressStart = Date.now()
-  const compressed = lz4.encode(csv)
-  const compressEnd = Date.now()
-
-  const decompressStart = Date.now()
-  const decompressed = csvToRecords(lz4.decode(compressed).toString())
-  const decompressEnd = Date.now()
-
-  assert(decompressed.length === records.length)
-
-  results['csv + lz4'] = {
-    sizeRatio: sizeRatio(compressed),
-    compressTime: compressEnd - compressStart,
-    decompressTime: decompressEnd - decompressStart
-  }
-}
-
 // Brotli + custom
 {
   const compressStart = Date.now()
@@ -181,7 +152,7 @@ const bencoded = bencode.encode(records)
     decompressTime: decompressEnd - decompressStart
   }
 
-  function encode(string) {
+  function encode (string) {
     const result = string.split(/\n/g).filter(Boolean).map(row => {
       const parts = row.split(',')
       const name = parts[0]
@@ -189,49 +160,73 @@ const bencoded = bencode.encode(records)
       const data = parts[4]
 
       return Buffer.from([
-        (() => {
+        ...(() => {
           switch (type) {
             case 'A':
-              return 1
+              return Buffer.from([0, 1])
             case 'NS':
-              return 2
+              return Buffer.from([0, 2])
+            case 'CNAME':
+              return Buffer.from([0, 5])
             case 'MX':
-              return 15
+              return Buffer.from([0, 15])
             case 'AAAA':
-              return 28
+              return Buffer.from([0, 28])
             case 'TXT':
-              return 16
+              return Buffer.from([0, 16])
             default:
-              return 255 // ANY
+              return Buffer.from([0, 255])
           }
         })(),
         ...Buffer.from(name),
         ...Buffer.from(' '),
-        ...Buffer.from(data),
+        ...(() => {
+          switch (type) {
+            case 'A':
+              return ipCodec.v4.encode(data)
+            case 'AAAA':
+              return ipCodec.v6.encode(data)
+            case 'TXT':
+              return Buffer.from(data.slice(1, data.length - 2))
+            default:
+              return Buffer.from(data)
+          }
+        })(),
         ...Buffer.from('\n')
       ])
     })
     return Buffer.concat(result)
   }
 
-  function decode(buf) {
+  function decode (buf) {
     const result = []
 
     let row = []
 
-    for (let x of buf) {
+    for (const x of buf) {
       if (x === Buffer.from('\n')[0]) {
-        const type = row[0]
-        const string = Buffer.from(row).toString().slice(1)
-        const name = string.split(' ')[0]
+        const type = row[1]
+        const rest = Buffer.from(row).subarray(2)
+        let name = []
 
-        result.push({
+        for (const d of rest) {
+          if (d === Buffer.from(' ')[0]) {
+            break
+          }
+          name.push(d)
+        }
+
+        name = Buffer.from(name).toString()
+
+        const _row = {
           type: (() => {
             switch (type) {
               case 1:
                 return 'A'
               case 2:
                 return 'NS'
+              case 5:
+                return 'CNAME'
               case 15:
                 return 'MX'
               case 28:
@@ -242,9 +237,22 @@ const bencoded = bencode.encode(records)
                 return 'ANY'
             }
           })(),
-          name,
-          data: string.slice(name.length + 1)
-        })
+          name: name.replace(/@$/, 'o4dksfbqk85ogzdb5osziw6befigbuxmuxkuxq8434q89uj56uyy.'),
+          data: (() => {
+            const _data = rest.subarray(name.length + 1)
+
+            switch (type) {
+              case 1:
+                return ipCodec.v4.decode(_data)
+              case 28:
+                return ipCodec.v6.decode(_data)
+              default:
+                return _data
+            }
+          })()
+        }
+
+        result.push(_row)
         row = []
       } else {
         row.push(x)
@@ -255,11 +263,11 @@ const bencoded = bencode.encode(records)
   }
 }
 
-function sizeRatio(compressed) {
+function sizeRatio (compressed) {
   return compressed.length / BASE
 }
 
-function csvToRecords(csv) {
+function csvToRecords (csv) {
   const records = csv
     .split(/\n/g).filter(Boolean)
     .map(
