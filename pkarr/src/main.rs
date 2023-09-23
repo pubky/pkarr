04b2bb39293d58b4e682b4328749b361f2ffd1e2
nazeh <ar.nazeh@gmail.com>
2023-09-23 21:48:06 +0300

    wip: create valid bep0044 request and send it to relay

--- pkarr/src/main.rs
+++ pkarr/src/main.rs
@@ -1,7 +1,10 @@
 #![allow(unused)]
 use crate::prelude::*;
 
+use ed25519_dalek::{Signature, SignatureError, Signer, SigningKey, VerifyingKey};
+use rand::rngs::OsRng;
 use reqwest::blocking;
+use std::time::SystemTime;
 use url::Url;
 
 mod error;
@@ -42,6 +45,89 @@ impl RelayClient {
 
         Some(bytes)
     }
+
+    fn put(self: RelayClient, bep44_put_args: &Bep44PutArgs) -> Result<()> {
+        let z32_key = zbase32::encode_full_bytes(&bep44_put_args.key);
+
+        let mut url = self.url.to_owned();
+        url.set_path(&z32_key);
+
+        dbg!(&url);
+
+        let client = reqwest::blocking::Client::new();
+
+        let response = match client.put(url).body(bep44_put_args.clone()).send() {
+            Ok(response) => response,
+            Err(err) => {
+                dbg!(err);
+                return Err(Error::Static("PUT request failed"));
+            }
+        };
+
+        match response.status() {
+            reqwest::StatusCode::OK => (),
+            reqwest::StatusCode::BAD_REQUEST => {
+                dbg!(response.bytes());
+            }
+            _ => (),
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Bep44PutArgs {
+    key: [u8; 32],
+    sequence: u64,
+    value: Vec<u8>,
+    /// The signable is the string `3:seqi{sequence}e1:v{value.len()}:{value}`
+    signable: Vec<u8>,
+    signature: [u8; 64],
+}
+
+impl Bep44PutArgs {
+    fn new_current(signer: SigningKey, value: Vec<u8>) -> Self {
+        let sequence = system_time_now();
+        let mut signable = format!("3:seqi{}e1:v{}:", sequence, value.len())
+            .as_bytes()
+            .to_vec();
+
+        signable.extend(&value);
+
+        dbg!(String::from_utf8(signable.clone()));
+
+        let signature = signer.sign(&signable);
+
+        Self {
+            key: signer.verifying_key().as_bytes().clone(),
+            sequence,
+            value,
+            signable: signable.into(),
+            signature: signature.to_bytes(),
+        }
+    }
+
+    fn clone(self: Self) -> Self {
+        Self {
+            key: self.key,
+            sequence: self.sequence.clone(),
+            value: self.value.clone(),
+            signable: self.signable.clone(),
+            signature: self.signature.clone(),
+        }
+    }
+}
+
+impl From<Bep44PutArgs> for reqwest::blocking::Body {
+    fn from(bep44_put_args: Bep44PutArgs) -> reqwest::blocking::Body {
+        let mut body = Vec::new();
+
+        body.extend_from_slice(&bep44_put_args.signature);
+        body.extend_from_slice(&bep44_put_args.signable);
+
+        reqwest::blocking::Body::from(body)
+    }
 }
 
 #[derive(Debug)]
@@ -49,6 +135,13 @@ struct Pkarr {
     // TODO: Use a single Client per [Pkarr] instance
     // TODO: add timeout to Client and test with a relay that does not end response stream.
     relays: Vec<RelayClient>,
+}
+
+fn system_time_now() -> u64 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("time drift")
+        .as_micros() as u64
 }
 
 impl Pkarr {
@@ -73,14 +166,28 @@ impl Pkarr {
 
         None
     }
+
+    fn publish(self: Pkarr, signer: SigningKey) -> Result<()> {
+        let sequence = system_time_now();
+        let value = bytes::Bytes::from("Hello world!");
+
+        let bep44_put_args = Bep44PutArgs::new_current(signer, value.into());
+
+        for relay in self.relays {
+            match relay.put(&bep44_put_args) {
+                Ok(bytes) => continue,
+                Err(_) => continue,
+            }
+        }
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
     let client = Pkarr::new();
 
-    dbg!(&client);
-
-    let x = client.resolve();
+    let x = client.publish(SigningKey::from([0; 32]));
 
     dbg!(x);
 
