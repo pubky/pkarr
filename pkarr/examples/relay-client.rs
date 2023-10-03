d77ba8a179fc4ba99587631bd357fcc18ceadaae
nazeh <ar.nazeh@gmail.com>
2023-10-03 16:42:46 +0300

    feat: display TXT records

--- pkarr/examples/relay-client.rs
+++ pkarr/examples/relay-client.rs
@@ -1,7 +1,7 @@
 //! This example shows how to publish and resolve [ResourceRecord]s to and from a Pkarr
 //! [relay](https://github.com/Nuhvi/pkarr/blob/main/design/relays.md).
 //!
-//! In this example, we publish both an A and CNAME records representing
+//! In this example, we publish both an A and CNAME records as well as a TXT record representing
 //! [Iroh](https://iroh.computer/) DERP regions used by Peer with the same Ed25519 key.
 //!
 //! For the purpose of this example we use the default Pkarr relay for both
@@ -50,6 +50,12 @@ async fn main() -> Result<()> {
                     .expect("is valid name")
                     .into(),
             ),
+        ));
+        packet.answers.push(dns::ResourceRecord::new(
+            dns::Name::new("_derp_region._iroh.").unwrap(),
+            dns::CLASS::IN,
+            30,
+            dns::rdata::RData::TXT("url=eu.derp.iroh.network:33000".try_into()?),
         ));
 
         let signed_packet = SignedPacket::from_packet(&keypair, &packet)?;
