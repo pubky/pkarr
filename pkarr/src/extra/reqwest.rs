6821166474cd567a1e11f39e335b42c0029d4063
nazeh <ar.nazeh@gmail.com>
2024-12-14 13:00:51 +0300

    fix(pkarr): reqwest Resolve implementation pass error from to_socket_addrs

--- pkarr/src/extra/reqwest.rs
+++ pkarr/src/extra/reqwest.rs
@@ -37,9 +37,5 @@ async fn resolve(
         return Ok(Box::new(addrs.into_iter()));
     };
 
-    Ok(Box::new(
-        format!("{name}:0")
-            .to_socket_addrs()
-            .expect("formatting a name and port to socket address"),
-    ))
+    Ok(Box::new(format!("{name}:0").to_socket_addrs()?))
 }
