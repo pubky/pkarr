0570a8cbeacad285c106c30917eb6696f6dc70cb
nazeh <ar.nazeh@gmail.com>
2025-02-17 13:08:07 +0300

    feat(relay): update the cache storage logic to be more clear

--- relay/src/config.rs
+++ relay/src/config.rs
@@ -16,13 +16,8 @@ use crate::rate_limiting::RateLimiterConfig;
 struct ConfigToml {
     http: Option<HttpConfig>,
     mainline: Option<MainlineConfig>,
-    cache_path: Option<String>,
-    cache_size: Option<usize>,
-    /// See [pkarr::ClientBuilder::minimum_ttl]
-    minimum_ttl: Option<u32>,
-    /// See [pkarr::ClientBuilder::maximum_ttl]
-    maximum_ttl: Option<u32>,
     rate_limiter: Option<RateLimiterConfig>,
+    cache: Option<CacheConfig>,
 }
 
 #[derive(Serialize, Deserialize, Default)]
@@ -33,6 +28,16 @@ struct HttpConfig {
 #[derive(Serialize, Deserialize, Default)]
 struct MainlineConfig {
     port: Option<u16>,
+}
+
+#[derive(Serialize, Deserialize, Default)]
+struct CacheConfig {
+    path: Option<PathBuf>,
+    size: Option<usize>,
+    /// See [pkarr::ClientBuilder::minimum_ttl]
+    minimum_ttl: Option<u32>,
+    /// See [pkarr::ClientBuilder::maximum_ttl]
+    maximum_ttl: Option<u32>,
 }
 
 /// Pkarr Relay configuration
@@ -77,6 +82,8 @@ impl Default for Config {
 impl Config {
     /// Load the config from a file.
     pub async fn load(path: impl AsRef<Path>) -> Result<Config> {
+        let config_file_path = path.as_ref();
+
         let s = tokio::fs::read_to_string(path.as_ref())
             .await
             .with_context(|| format!("failed to read {}", path.as_ref().to_string_lossy()))?;
@@ -85,12 +92,31 @@ impl Config {
 
         let mut config = Config::default();
 
-        if let Some(ttl) = config_toml.minimum_ttl {
-            config.pkarr.minimum_ttl(ttl);
-        }
+        if let Some(cache_config) = config_toml.cache {
+            if let Some(ttl) = cache_config.minimum_ttl {
+                config.pkarr.minimum_ttl(ttl);
+            }
+            if let Some(ttl) = cache_config.maximum_ttl {
+                config.pkarr.maximum_ttl(ttl);
+            }
 
-        if let Some(ttl) = config_toml.maximum_ttl {
-            config.pkarr.maximum_ttl(ttl);
+            if let Some(cache_path) = cache_config.path.as_ref() {
+                config.cache_path = Some(if cache_path.is_relative() {
+                    // support relative path.
+                    config_file_path
+                        .canonicalize()
+                        .expect("valid config path")
+                        .parent()
+                        .unwrap_or_else(|| Path::new("."))
+                        .join(cache_path)
+                        .canonicalize()
+                        .expect("valid cache path")
+                } else {
+                    cache_path.clone()
+                });
+            }
+
+            config.cache_size = cache_config.size.unwrap_or(DEFAULT_CACHE_SIZE);
         }
 
         if let Some(port) = config_toml.mainline.and_then(|m| m.port) {
@@ -101,11 +127,6 @@ impl Config {
             config.http_port = port;
         }
 
-        if let Some(path) = config_toml.cache_path {
-            config.cache_path = Some(PathBuf::from(path).join(CACHE_DIR));
-        }
-
-        config.cache_size = config_toml.cache_size.unwrap_or(DEFAULT_CACHE_SIZE);
         config.rate_limiter = config_toml.rate_limiter;
 
         Ok(config)
