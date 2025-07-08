c1815308de58e47e6058adc7db8efec3eba29d66
nazeh <nuh@nuh.dev>
2025-07-08 11:53:18 +0300

    chore: fix cargo check issues

--- bindings/js/build.rs
+++
@@ -1,9 +0,0 @@
-use cfg_aliases::cfg_aliases;
-
-fn main() {
-    // Convenience aliases
-    cfg_aliases! {
-        wasm_browser: { all(target_family = "wasm", target_os = "unknown") },
-        relays: { feature = "relays" },
-    }
-}
