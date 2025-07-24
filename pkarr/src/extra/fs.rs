5952bf29fbda780120a734632f28dbe5cbe51184
ok300 <106775972+ok300@users.noreply.github.com>
2025-07-24 14:01:34 +0200

    Use &Path instead of PathBuf

--- pkarr/src/extra/fs.rs
+++ pkarr/src/extra/fs.rs
@@ -2,13 +2,13 @@
 
 #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
-use std::path::PathBuf;
+use std::path::Path;
 
 use crate::Keypair;
 
 /// Writes the secret key of the keypair to file, as a hex encoded string.
 /// If the file already exists, it will be overwritten.
-pub fn write_keypair(keypair: &Keypair, secret_file_path: PathBuf) -> Result<(), std::io::Error> {
+pub fn write_keypair(keypair: &Keypair, secret_file_path: &Path) -> Result<(), std::io::Error> {
     let secret = keypair.secret_key();
     let hex_string = const_hex::encode(secret);
     std::fs::write(secret_file_path.clone(), hex_string)?;
